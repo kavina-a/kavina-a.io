a49edcd7d19071988c1c42b4963abdfc9bdc671c
Kavina <kamnthuka@gmail.com>
2024-10-10 11:31:41 +0530

    vite updated

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/MyPortfolio/',  // Replace with your actual repository name
+  base: './', // Use '/' for root URL, which is appropriate for custom domains
   plugins: [react()],
-});
+})
