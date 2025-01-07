d7fe241a87b5093eadcfe23d3cddb35b2b516e04
kavina-a <kmanthuka@gmail.com>
2025-01-07 12:43:48 +0530

    added new projects

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -6,20 +6,34 @@ import { FaGithub } from 'react-icons/fa';
 const projects = [
   {
     id: 1,
+    name: "Earthify",
+    image: "/earthify-cover.png",
+    description: "Earthify is your gateway to a greener lifestyle. Discover eco-friendly products, earn Eco Points, and unlock discounts while contributing to a sustainable future. Built with innovation and purpose, Earthify doesn’t just sell products—it empowers change. ",
+    link: 'https://github.com/kavina-a/FixItNow.git',
+  },
+  {
+    id: 2,
+    name: "Petfinity",
+    image: "/Petfinity.png",
+    description: "Petfinity is not just another pet app—it’s your ultimate companion for everything pet-related. Built with cutting-edge technology, this all-in-one platform connects pet owners, trainers, and boarders like never before. From seamless appointment bookings to real-time notifications, lost-and-found services, and detailed pet profiles, Petfinity makes caring for your furry friends effortless and stress-free. The future of pet parenting starts here.",
+    link: 'https://github.com/kavina-a/FixItNow.git',
+  },
+  {
+    id: 3,
     name: "Flutter Mobile App",
     image: "/fixitnow.png",
     description: "Need help with home repairs, landscaping, or moving? Meet FixItNow – your go-to app for finding and booking trusted manual labor professionals in just a few taps. With FixItNow, you can browse profiles, read reviews, and book reliable services quickly, all from a user-friendly platform designed to simplify your life.",
     link: 'https://github.com/kavina-a/FixItNow.git',
   },
   {
-    id: 2,
+    id: 4,
     name: "Inventory Management System",
     image: "/inventory.png",
     description: "A comprehensive Stock Management System using C# and the MySQL to address specific requirements in stock management. This project involved to develop two main functionalities: adding new stock items and updating stock quantities.",
     link: 'https://github.com/kavina-a/Stock-Management-System.git',
   },
   {
-    id: 3,
+    id: 5,
     name: "Ceylon Turtle ORG",
     image: "/ceylonTurtleOrg.png",
     description: "During my first year at APIIT, I developed a web platform called Ceylon Turtle Org as part of an assignment. This project focuses on promoting Sri Lanka's turtle sanctuaries in Hikkaduwa, Kosgoda, and Bentota. The initial website was created using HTML and CSS, and in the second semester, I enhanced the functionality by adding a ticket booking feature using JavaScript.",
